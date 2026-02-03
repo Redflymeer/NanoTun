0ab78efc4ebbf6d20911b55cd26da753131f4c9e
rfm <redflymeer@gmail.com>
2026-02-03 09:57:28 +0500

    Added to script shadowsocks parsing

--- core/NanoBurn.cpp
+++ core/NanoBurn.cpp
@@ -1,3 +1,10 @@
-#include <sodium.h>
-#include <openssl/evp.h>
-#include <openssl/hmac.h>
+#include "NanoBurn.h"
+#include <net/if.h>  // TUN
+#include <sodium.h>  // ChaCha20-Poly [web:8][web:11]
+#include <base64.h>  // URI decode [web:9]
+
+bool ss_start(const std::string& uri) {
+    // sodium_init(); chacha20poly1305_encrypt()
+    // tun_setup("tun0"); proxy_loop();
+    return true;  // TODO
+}
