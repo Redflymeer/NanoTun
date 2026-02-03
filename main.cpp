b3eb117cd3bf7005517cb3eb3bd4aaa253040c74
rfm <redflymeer@gmail.com>
2026-02-03 15:13:50 +0500

    CHANGED TO FULL WORK SHADOWSOCKS

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include "core/NanoBurn.h"
-#include <iostream>
-int main(int argc, char** argv) {
-    if (argc != 2) { std::cout << "Usage: ./NanoTun 'ss://...'\n"; return 1; }
-    ss_start(argv[1]);  // Turning on 
-    std::cout << "SS on (Ctrl+C off)\n";
-    std::cin.get();
+
+int main() {
+    NanoTun::SSConfig config;
+    config.method = "chacha20-ietf-poly1305";
+    config.password = "mypass";
+    config.server = "1.2.3.4";
+    config.port = 8388;
+    
+    NanoTun::NanoBurn::init(config);
+    NanoTun::NanoBurn::start();
     return 0;
 }
