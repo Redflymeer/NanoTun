0d95d8c914760cf6de496bc16de7f1a5301172f9
rfm <redflymeer@gmail.com>
2026-02-03 15:31:08 +0500

    Added input url and vless support

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 #include "core/NanoBurn.h"
+#include <iostream>
 
 int main() {
-    NanoTun::SSConfig config;
-    config.method = "chacha20-ietf-poly1305";
-    config.password = "mypass";
-    config.server = "1.2.3.4";
-    config.port = 8388;
+    std::string url;
+    std::cout << "Enter URL (ss:// or vless://): ";
+    std::getline(std::cin, url);
     
-    NanoTun::NanoBurn::init(config);
+    if (url.substr(0, 5) == "ss://") {
+        auto ss = NanoTun::NanoBurn::parse_ss_url(url);
+        NanoTun::NanoBurn::init(ss);
+    } else if (url.substr(0, 8) == "vless://") {
+        auto vless = NanoTun::NanoBurn::parse_vless_url(url);
+        NanoTun::NanoBurn::init(vless);
+    }
+    
     NanoTun::NanoBurn::start();
     return 0;
 }
