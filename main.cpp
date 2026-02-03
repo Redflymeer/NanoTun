a603c906d28888fcd7798bb4deefe125c990882d
rfm <redflymeer@gmail.com>
2026-02-03 10:00:12 +0500

    Create main.cpp and shadowsocks activate

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "сore/NanoBurn.h"
+#include <iostream>
+int main(int argc, char** argv) {
+    if (argc != 2) { std::cout << "Usage: ./NanoTun 'ss://...'\n"; return 1; }
+    ss_start(argv[1]);  // Turning on 
+    std::cout << "SS on (Ctrl+C off)\n";
+    pause();  // loop
+    return 0;
+}
