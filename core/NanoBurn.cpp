9e4c2f4ddff5934f34dcf2debaf51c7b86aaa75a
rfm <redflymeer@gmail.com>
2026-02-03 10:08:57 +0500

    Changed base64.h to simple_base64.h

--- core/NanoBurn.cpp
+++ core/NanoBurn.cpp
@@ -1,7 +1,7 @@
-#include "NanoBurn.h"
+#include <core/NanoBurn.h>
 #include <net/if.h>  // TUN
 #include <sodium.h>  // ChaCha20-Poly [web:8][web:11]
-#include <base64.h>  // URI decode [web:9]
+#include <core/simple_base64.h>  // URI decode [web:9]
 
 bool ss_start(const std::string& uri) {
     // sodium_init(); chacha20poly1305_encrypt()
